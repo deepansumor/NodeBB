f41ee5f539c5346832895a85e9f88ef911093c24
Deepansu Mor <deepansumor@gmail.com>
2025-05-28 18:30:01 +0530

    Update create.js

--- src/topics/create.js
+++ src/topics/create.js
@@ -17,7 +17,7 @@ const privileges = require('../privileges');
 const categories = require('../categories');
 const translator = require('../translator');
 
-const EXTRA_FIELDS = ['adType', 'cadence', 'summary', 'status', 'stage', 'escalationDate', 'portfolioName'];
+const EXTRA_FIELDS = ['adType', 'cadence', 'summary', 'status', 'stage', 'escalationDate', 'portfolio'];
 
 
 module.exports = function (Topics) {
