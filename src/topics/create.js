58297f3f65b8c0deff4a8c79047d21c50fc3e01a
Ravinder Choudhary <ravinderchoudhary.dev@gmail.com>
2025-07-09 13:22:02 +0530

    Update SCSS imports and add new PDP audit styles; modify EXTRA_FIELDS in create.js

--- src/topics/create.js
+++ src/topics/create.js
@@ -17,7 +17,7 @@ const privileges = require('../privileges');
 const categories = require('../categories');
 const translator = require('../translator');
 
-const EXTRA_FIELDS = ['adType', 'cadence', 'summary', 'status', 'stage', 'escalationDate', 'portfolio'];
+const EXTRA_FIELDS = ['adType', 'cadence', 'summary', 'status', 'stage', 'escalationDate', 'portfolioId','portfolioName'];
 
 
 module.exports = function (Topics) {
