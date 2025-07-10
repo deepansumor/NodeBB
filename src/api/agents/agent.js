c56ccfbc95c82a7d1ed86086b997804c7c7b2a06
bawanetejas <tejasbawane221@gmail.com>
2025-07-10 18:55:24 +0530

    refactor: Comment out unused imports for clarity

--- src/api/agents/agent.js
+++ src/api/agents/agent.js
@@ -1,7 +1,7 @@
 const services = require("../../services");
 const db = require("../../database");
 const COLLECTIONS = require("../../database/mongo/collections");
-const prompts = require("../../services/prompts");
+// const prompts = require("../../services/prompts");
 const nconf = require("nconf");
 const AuditOrchestrator = require("../../services/agentSuite/orchestrator")
 // Create orchestrator instance
--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,11 @@
 const services = module.exports;
 const RainForsetAPI = require("./rainforset")
 const AwsS3 = require("./aws-s3")
-const Gemini = require("./gemini")
+// const Gemini = require("./gemini")
 const SPapi = require("./spapi")
 const authorization = require("./authorize")
 services.rainforest = RainForsetAPI;
 services.s3api = AwsS3
-services.gemini = Gemini
+// services.gemini = Gemini
 services.spapi = SPapi
 services.authorization = authorization
